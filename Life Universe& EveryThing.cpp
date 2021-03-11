b79456f4e629a19ab69d123d1ca06d5a2d8f50f2
JithendraNath Eswar <66664321+JithendraNathEswaR@users.noreply.github.com>
2021-03-11 09:42:32 +0530

    Create Life Universe& EveryThing.cpp

---
+++ Life Universe& EveryThing.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    while(true){   #to run infinite loop
+        int n;
+        cin>>n;
+        if(n==42){
+            break;
+        }
+        cout<<n<<endl;
+    }
+}
+
+# Sample Input:
+1
+2
+88
+42
+99
+
+
+#Sample Output:
+1
+2
+88
+
